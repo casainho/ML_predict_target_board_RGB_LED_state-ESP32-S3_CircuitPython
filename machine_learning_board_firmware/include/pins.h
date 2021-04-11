dbdacd04040e0a25bad7f3db6f20314ff745d554
casainho <casainho@gmail.com>
2021-04-11 16:38:30 +0100

    machine learning firmware BLE UART works

---
+++ machine_learning_board_firmware/include/pins.h
@@ -0,0 +1,25 @@
+/*
+ * NRF52840_detect_LED_flashing_by_observing_current
+ *
+ * Copyright (C) Casainho, 2021
+ *
+ * Released under the GPL License, Version 3
+ * 
+ */
+
+#ifndef PINS_H_
+#define PINS_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "nrf_gpio.h"
+
+void pins_init(void);
+
+typedef enum {
+  LED_R__PIN  = 8,
+  LED_G__PIN  = 32 + 9, // P1.9
+  LED_B__PIN  = 12,
+} LED_pins_t;
+
+#endif /* PINS_H_ */
